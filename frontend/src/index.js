c1b7da6f5ac5be360b3840be65b6874c546361b0
Nargiza Sainazarova <nargizasaina@gmail.com>
2022-10-21 09:58:06 +0600

    Initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const app = (
+    <App/>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(app);
